d4cc95439dfa1a12aa147ed4030089d939bb64b1
MouadAgn <aoughanemouad1@gmail.com>
2023-12-21 16:08:26 +0100

    Creation de la table intermediare option_module

---
+++ routes/option_modele_route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const option_modele_controller = require('../controllers/option_modele_controller');
+/*
+router.get('/opmodassocier', option_modele_controller.getAllOptionModele);
+*/
+module.exports = router; 
